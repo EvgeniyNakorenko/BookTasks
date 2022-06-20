285a9d3f1329d6e1dc7efe7b50a1e2e384b00c01
EvgeniyNakorenko <nakorenko_zheka@mail.ru>
2022-06-20 16:46:11 +0300

    added lastbutton in Book

--- Book/app/src/main/java/com/example/kalkul/MainActivity.kt
+++ Book/app/src/main/java/com/example/kalkul/MainActivity.kt
@@ -6,11 +6,13 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
+    private lateinit var lastButton: Button
     private lateinit var questionTextView: TextView
     private val questionBank = listOf(
         Question(R.string.question_australia,true),
@@ -28,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        lastButton = findViewById(R.id.last_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
@@ -38,6 +41,10 @@ class MainActivity : AppCompatActivity() {
             checkAnswer(false)
         }
         nextButton.setOnClickListener {
+            currentIndex = (abs(currentIndex - 1)) % questionBank.size
+            updateQuestion()
+        }
+        lastButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
         }
